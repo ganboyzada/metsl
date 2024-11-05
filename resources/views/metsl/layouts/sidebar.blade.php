4d8c498d1618a646137a3dcdecba9fcbee810369
ganboyzada <95960364+ganboyzada@users.noreply.github.com>
2024-11-05 18:53:06 +0400

    logo

--- resources/views/metsl/layouts/sidebar.blade.php
+++ resources/views/metsl/layouts/sidebar.blade.php
@@ -1,23 +1,23 @@
 <aside class="w-64 bg-gray-800 text-white flex flex-col">
-    <div class="p-4 text-center text-2xl font-bold">
-        Project Manager
+    <div class="py-4 px-5 w-100">
+        <img src="{{ asset('images/logo-light.png') }}" class="object-contain h-10" alt="">
     </div>
     <nav class="flex-1">
         <ul>
-            <li class="flex items-center py-2 px-4 hover:bg-gray-700">
-                <i class="fas fa-building mr-2 text-gray-400"></i>
+            <li class="flex items-center py-3 px-5 hover:bg-gray-700">
+                <i class="fas fa-building mr-3 text-gray-400"></i>
                 <a href="#" class="text-white">Entities</a>
             </li>
-            <li class="flex items-center py-2 px-4 hover:bg-gray-700">
-                <i class="fas fa-city mr-2 text-gray-400"></i>
+            <li class="flex items-center py-3 px-5 hover:bg-gray-700">
+                <i class="fas fa-city mr-3 text-gray-400"></i>
                 <a href="{{ route('companies') }}" class="text-white">Companies</a>
             </li>
-            <li class="flex items-center py-2 px-4 hover:bg-gray-700">
-                <i class="fas fa-cogs mr-2 text-gray-400"></i>
+            <li class="flex items-center py-3 px-5 hover:bg-gray-700">
+                <i class="fas fa-cogs mr-3 text-gray-400"></i>
                 <a href="#" class="text-white">Settings</a>
             </li>
-            <li class="flex items-center py-2 px-4 hover:bg-gray-700">
-                <i class="fas fa-file-alt mr-2 text-gray-400"></i>
+            <li class="flex items-center py-3 px-5 hover:bg-gray-700">
+                <i class="fas fa-file-alt mr-3 text-gray-400"></i>
                 <a href="#" class="text-white">Documents</a>
             </li>
         </ul>
