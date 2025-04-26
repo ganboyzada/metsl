e0914690902c4b368280b11ca318e28c4155e970
marina3mad100 <marinaemad100@gmail.com>
2025-04-26 12:51:37 +0200

    26-04

--- app/Http/Controllers/Api/V1/SnagListController.php
+++ app/Http/Controllers/Api/V1/SnagListController.php
@@ -217,6 +217,7 @@ class SnagListController extends Controller
 
                 $all_data['status'] = 0;
                 $all_data['date_notified_at'] = date('Y-m-d');
+                dd($all_data);
                 $model = $this->punchListService->create($all_data);
             \DB::commit();
             // all good
