9e08bb7f6f069db44a9be7a8eb18740ad6d2ed7b
marina3mad100 <marinaemad100@gmail.com>
2025-05-06 16:17:35 +0200

    06-05

--- resources/views/metsl/pages/correspondence/index.blade.php
+++ resources/views/metsl/pages/correspondence/index.blade.php
@@ -72,6 +72,7 @@
 <script>
 	$(".projectButton").on('click',function(event) {
 		loadedRows = 0;
+        alert('ok');
 		if(localStorage.getItem("project_tool") == 'correspondence'){
 			get_correspondences();
 		}
