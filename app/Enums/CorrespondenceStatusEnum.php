543c9ed92554a3d9eb7ee0cdaa5b5aa7046c1d1c
marina3mad100 <marinaemad100@gmail.com>
2025-05-06 13:37:05 +0200

    06-05

--- app/Enums/CorrespondenceStatusEnum.php
+++ app/Enums/CorrespondenceStatusEnum.php
@@ -4,7 +4,6 @@ namespace App\Enums;
 
 enum  CorrespondenceStatusEnum: string
 {
-    case DRAFT = 'Draft';
 
     case ISSUED = 'Issued';
 
@@ -16,7 +15,6 @@ enum  CorrespondenceStatusEnum: string
     {
         return match($this) 
         {
-            self::DRAFT => 'bg-gray-200 text-black',
             self::ISSUED => 'bg-yellow-500 text-black',
             self::OPEN => 'bg-blue-500 text-white',
             self::CLOSED => 'bg-green-500 text-white',
