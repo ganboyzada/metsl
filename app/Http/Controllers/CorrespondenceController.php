281f67af5aec1934ce165b142325cef788e2be68
marina3mad100 <marinaemad100@gmail.com>
2025-05-06 17:43:37 +0200

    06-05

--- app/Http/Controllers/CorrespondenceController.php
+++ app/Http/Controllers/CorrespondenceController.php
@@ -149,7 +149,7 @@ class CorrespondenceController extends Controller
 
     public function ProjectCorrespondence(Request $request){
         $id = Session::get('projectID');
-       // dd(session('projectID'));
+        dd(session('projectID'));
         $correspondeces = $this->correspondenceService->getAllProjectCorrespondence($id , $request);
         $correspondeces->map(function($row){
             return $row->status_color = [CorrespondenceStatusEnum::from($row->status) , CorrespondenceStatusEnum::from($row->status)->color()];
